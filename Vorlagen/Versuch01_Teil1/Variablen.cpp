fa05a6fb41017299979a866ec58748930aede552
umutkarab <133097185+umutkarab@users.noreply.github.com>
2023-05-18 21:51:11 +0200

    Add files via upload

---
+++ Vorlagen/Versuch01_Teil1/Variablen.cpp
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////////////////
+// Praktikum Informatik 1 MMXXIII
+// Versuch 1.1: Datentypen und Typumwandlung
+//
+// Datei:  Variablen.cpp
+// Inhalt: Hauptprogramm
+//////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int iErste = 0;
+    int iZweite = 0;
+    
+    // Hier folgt Ihr eigener Code
+    
+    return 0;
+    
+}
---
+++ Vorlagen/Versuch02_Teil3/Verschluesselung.cpp
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////
+// Praktikum Informatik 1 MMXXIII
+// Versuch 2.3: Felder
+//
+// Datei:  Verschuesselung.cpp
+// Inhalt: Hauptprogramm
+//////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+#include <iostream>
+#include <string>
+
+string verschluesseln(char schluessel[2][26], string wort)
+{
+	// Hier folgt Ihr Code
+
+	return wort;
+}
+
+string entschluesseln(char schluessel[2][26], string wort)
+{
+	// Hier folgt Ihr Code
+
+	return wort;
+}
+
+int main()
+{
+	// Hier folgt Ihr Code
+
+	return 0;
+}
